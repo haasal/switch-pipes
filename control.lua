2a344f7d7885d782f430ac60b43f4a1c9cefd3af
Alexander Haas <mail@haas-alexander.com>
2023-03-31 22:51:15 +0200

    refactor: removed the outdated on_gui_opened event

--- control.lua
+++ control.lua
@@ -5,9 +5,3 @@ commands.add_command("cheatitems", nil, function(command)
     game.player.insert { name = "infinity-pipe", count = 10 }
     game.player.insert { name = "electric-energy-interface", count = 50 }
 end)
-
-script.on_event(defines.events.on_gui_opened, function(event)
-    if not event then return end
-    if not event.entity then return end
-    if event.entity.name ~= "oil-refinery" then return end
-end)
